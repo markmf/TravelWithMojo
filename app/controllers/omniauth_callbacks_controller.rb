8ff55fad90d1210711ca73f2bce5a530370749f9
Mark Francisco <markmf@yahoo.com>
2017-09-09 22:01:57 +0900

    aded html styling

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -15,6 +15,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def google_oauth2
 	  @user = User.from_omniauth(request.env["omniauth.auth"])  
 
+	  logger.debug "USER INFO ==>  #{@user.inspect}"
+
 	  if @user.persisted?
 	      sign_in_and_redirect @user, :event => :authentication
 	      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -279,6 +279,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  #OmniAuth.config.full_host = Rails.env.production? ? 'https://www.eyeforwonder.com' : 'http://localhost:3000' 
   config.omniauth :facebook, ENV['FB_APP_CLIENT_ID'], ENV['FB_APP_CLIENT_SECRET'], scope: 'email', info_fields: 'email, name'
   config.omniauth :google_oauth2, ENV['GOOGLE_APP_CLIENT_ID'], ENV['GOOGLE_APP_CLIENT_SECRET']
   config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
